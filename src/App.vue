5a319df78446378d9cadbdc3219eb8fdf3497bb4
fengtianxiang <fengtianxiang@youzan.com>
2020-01-11 15:27:20 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to FTX's Vue.js App" />
+    <test />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld';
+import Test from './components/Test';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Test
   }
-}
+};
 </script>
 
 <style>
---
+++ src/components/Test.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <p>Hello World</p>
+    <p>I'm FTX</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
